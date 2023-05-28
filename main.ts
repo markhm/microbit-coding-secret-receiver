b09cc52e0ff008f1c0948ca1924cc34ad66a0b33
Mark HM <github@hissinkmuller.nl>
2023-05-28 11:09:16 +0200

    Receiving channel

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 radio.onReceivedString(function (receivedString) {
+    let current_channel = 0
+    match_channel = current_channel
     music.ringTone(262)
     basic.showIcon(IconNames.Diamond)
+    basic.pause(1000)
+    while (true) {
+        basic.showString("Channel")
+        basic.showNumber(match_channel)
+    }
 })
+let match_channel = 0
 basic.showLeds(`
     . . . . .
     . . . . .
